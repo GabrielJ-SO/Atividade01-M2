7626a6713f0d458e31a9f344891f6a5d1f352154
GabrielJ-SO <gabrielodisi20@gmail.com>
2025-09-30 15:54:00 -0300

    Algumas modificações

--- main.py
+++ main.py
@@ -11,20 +11,120 @@ while continuar:
        
             
         case "1":
+
             print("\tExercício 1 - Bonus salario\n")
 
             salario = float(input("Digite o seu salário:"))
             tempo = int(input("Digite o tempo de empresa:"))
 
-            utils.CalculoBonusSalario(salario, tempo)
+            salarioFinal = utils.CalculoBonusSalario(salario, tempo)
+
+            print(f"O seu salário com o bônus é: R$ {salarioFinal:.2f}")
             
             input("Pressione Enter para continuar...")
             continue
+
         case "2":
-            print("Exercício 2 - identificador de triângulo")
+            
+            print("\tExercício 2 - Identificador de triângulo\n")
+
+            lado1 = float(input("Digite o comprimento do primeiro lado: "))
+            lado2 = float(input("Digite o comprimento do segundo lado: "))
+            lado3 = float(input("Digite o comprimento do terceiro lado: "))
+
+            tipo_triangulo = utils.IdentificadorTriangulo(lado1, lado2, lado3)
+            print(f"O triângulo é: {tipo_triangulo}")
+
+            input("Pressione Enter para continuar...")
             continue
+
+        case "3":
+
+            print("\tExercício 3 - Calculadora de IMC\n")
+
+            peso = float(input("Digite o seu peso (kg): "))
+            altura = float(input("Digite a sua altura (m): "))
+
+            classificacao = utils.CalculadoraIMC(peso, altura)
+            print(f"Sua classificação de IMC é: {classificacao}")
+
+            input("Pressione Enter para continuar...")
+            continue
+
+        case "4":
+
+            print("\tExercício 4 - Calculo de média\n")
+
+            notas = [0, 0, 0]
+
+            for i in range(3):
+                notas[i] = float(input(f"Digite a  {i+1}° nota: "))
+
+                if(notas[i] == 0):
+                    break
+
+            media = utils.CalculaMedia(notas)
+
+            if(media >= 7):
+                print("Aluno aprovado!")
+
+            elif(media >= 5 and media < 7):
+                print("Aluno em recuperação!")
+
+            elif(media < 5):
+                print("Aluno reprovado!")
+
+            input("Pressione Enter para continuar...")
+            continue
+
+        case "5":
+
+            print("\tExercício 5 - Verificador de data valida\n")
+
+
+
+            input("Pressione Enter para continuar...")
+            continue
+
+        case "6":
+            print("\tExercício 6 - Verifica condição de voto\n")
+
+
+
+            input("Pressione Enter para continuar...")
+            continue
+
+        case "7":
+
+            print("\tExercício 7 - Classificador de risco\n")
+
+
+
+            input("Pressione Enter para continuar...")
+            continue
+        
+        case "8":
+
+            print("\tExercício 8 - Jogo de adivinhação\n")
+
+
+
+            input("Pressione Enter para continuar...")
+            continue
+
+        case "9":
+
+            print("\tExercício 9 - Desconto para cliente VIP\n")
+
+
+
+            input("Pressione Enter para continuar...")
+            continue
+
         case "10":
-            print("Exercício 10 - Validador de senha")
+
+            print("\tExercício 10 - Validador de senha\n")
+
             continue
         case "0":
             print("Saindo...")
--- utils.py
+++ utils.py
@@ -1,5 +1,11 @@
 
+
 def menu():
+    """Exibe o menu e retorna a opção selecionada.
+    
+        Returns:
+            str: A opção selecionada pelo usuário.
+    """
     print("\tMenu de opções:")
     print(".1 - Bonus salario")
     print(".2 - identificador de triângulo")
@@ -16,9 +22,21 @@ def menu():
 
     return opcao
 
-def CalculoBonusSalario(salario, tempo):
 
 
+def CalculoBonusSalario(salario, tempo) -> float:
+   
+    """Calcula o bônus salarial com base no salário e tempo de empresa.
+    
+    Args:
+        salario (float): O salário atual do funcionário.
+        tempo (int): O tempo de empresa em anos.
+
+    Returns:
+        float: O salário final com o bônus aplicado.
+    """
+
+    bonus = 0
     if salario < 2000 and tempo >= 5:
         bonus = salario * 0.2
 
@@ -36,4 +54,85 @@ def CalculoBonusSalario(salario, tempo):
 
     salario += bonus
 
-    print("O seu salário é:", salario)
+    return salario
+
+
+
+def IdentificadorTriangulo(lado1, lado2, lado3) -> str:
+    """Identifica o tipo de triângulo com base nos comprimentos dos lados.
+    
+    Args:
+        lado1 (float): Comprimento do primeiro lado.
+        lado2 (float): Comprimento do segundo lado.
+        lado3 (float): Comprimento do terceiro lado.
+
+    Returns:
+        str: O tipo de triângulo ("Equilátero", "Isósceles", "Escaleno" ou "Não é um triângulo").
+    """
+
+    if (lado1 + lado2 < lado3) or (lado2 + lado3 < lado1) or (lado1 + lado3 < lado2):
+        return "Não é um triângulo"
+    
+    elif lado1 == lado2 and lado3 == lado1:
+        return "Equilátero"
+    
+    elif lado1 != lado2 and lado2 != lado3 and lado3 != lado1:
+        return "Escaleno"
+    
+    else:
+        return "Isósceles"
+    
+
+
+def CalculadoraIMC(peso, altura) -> str:
+
+    """Calcula o Índice de Massa Corporal (IMC) e classifica o resultado.
+    
+        Args:
+            peso (float): O peso em quilogramas.
+            altura (float): A altura em metros.
+
+        Returns:
+            str: A classificação do IMC.
+    """
+
+    imc = peso / (altura ** 2)
+
+    if(imc < 18.5):
+        return "Abaixo do peso"
+    
+    elif(imc >= 18.5 and imc < 25):
+        return "Peso normal"
+    
+    elif(imc >= 25 and imc < 30):
+        return "Sobrepeso"
+    
+    elif(imc >= 30 and imc < 35):
+        return "Obesidade grau I"
+    
+    elif(imc >= 35 and imc < 40):
+        return "Obesidade grau II"
+    
+    else:
+        return "Obesidade grau III"
+
+
+
+def CalculaMedia(notas) -> float:
+    """Calcula a média de uma lista de notas.
+    
+        Args:
+            notas (list of float): Lista contendo as notas.
+
+        Returns:
+            float: A média das notas.
+    """
+    media = 0
+    for i in range(len(notas)):
+        media += notas[i]
+
+    media = media / len(notas)
+
+    return media
+
+
